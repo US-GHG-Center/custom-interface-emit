d3253e96a83730160d2c7d3d4eb1d17a4e3d4dd6
Ram Sharan Rimal <rsr0012@uah.edu>
2025-06-15 23:15:39 -0500

    combine package json for lib and app

--- lib/src/index.ts
+++
@@ -1,5 +0,0 @@
-export { EmitInterface } from '../../src/pages/emitInterface';
-export type {
-  EmitInterfaceProps,
-  EmitInterfaceConfig,
-} from '../../src/pages/emitInterface/types';
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-export { EmitInterface } from './pages/emitInterface';
-export type { EmitInterfaceProps,EmitInterfaceConfig } from './pages/emitInterface/types';
-
