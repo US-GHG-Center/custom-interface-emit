4d9739f3fef1f12b012ad9aa28c63a7ca0d2787a
Ram Sharan Rimal <rsr0012@uah.edu>
2025-06-25 10:27:30 -0500

    fix emit component

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,10 @@ import { EmitInterface } from './pages/emitInterface';
 import './App.css';
 
 const BASE_PATH = process.env.PUBLIC_URL;
-
+const defaultCollectionId = 'emit-ch4plume-v1';
+const defaultZoomLocation = [-98.771556, 32.967243];
+const defaultZoomLevel = 4;
+const defaultStartDate = '2022-08-22';
 function App() {
   return (
     <Fragment>
@@ -16,7 +19,17 @@ function App() {
       <LocalizationProvider dateAdapter={AdapterMoment}>
         <BrowserRouter basename={BASE_PATH}>
           <Routes>
-            <Route path='/' element={<EmitInterface />} />
+            <Route
+              path='/'
+              element={
+                <EmitInterface
+                  defaultCollectionId={defaultCollectionId}
+                  defaultZoomLocation={defaultZoomLocation}
+                  defaultZoomLevel={defaultZoomLevel}
+                  defaultStartDate={defaultStartDate}
+                />
+              }
+            />
           </Routes>
         </BrowserRouter>
       </LocalizationProvider>
--- src/pages/emitInterface/index.jsx
+++ src/pages/emitInterface/index.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { DashboardContainer } from '../dashboardContainer';
 import { ConfigProvider } from '../../context/configContext';
 
-const defaultCollectionId = 'emit-ch4plume-v1';
-const defaultZoomLocation = [-98.771556, 32.967243];
-const defaultZoomLevel = 4;
-const defaultStartDate = '2022-08-22';
-export function EmitInterface({ config = {} }) {
+export function EmitInterface({
+  config = {},
+  defaultCollectionId,
+  defaultZoomLocation,
+  defaultZoomLevel,
+  defaultStartDate,
+}) {
   return (
     <ConfigProvider userConfig={config}>
       <DashboardContainer
