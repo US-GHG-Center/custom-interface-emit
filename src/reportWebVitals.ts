590565517cbde58b2a7158cc79abcb73a2c98d11
Ram Sharan Rimal <rsr0012@uah.edu>
2025-07-29 14:49:20 -0500

    fix error of webvitals

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,19 +1,14 @@
-// src/reportWebVitals.ts
-import { MetricType } from 'web-vitals';
+import { ReportHandler } from 'web-vitals';
 
-const reportWebVitals = (onPerfEntry?: (metric: MetricType) => void): void => {
-  if (onPerfEntry && typeof onPerfEntry === 'function') {
-    import('web-vitals')
-      .then(({ onCLS, onINP, onFCP, onLCP, onTTFB }) => {
-        onCLS(onPerfEntry);
-        onINP(onPerfEntry);
-        onFCP(onPerfEntry);
-        onLCP(onPerfEntry);
-        onTTFB(onPerfEntry);
-      })
-      .catch((err) => {
-        console.error('Failed to load web-vitals module:', err);
-      });
+const reportWebVitals = (onPerfEntry?: ReportHandler) => {
+  if (onPerfEntry && onPerfEntry instanceof Function) {
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      getCLS(onPerfEntry);
+      getFID(onPerfEntry);
+      getFCP(onPerfEntry);
+      getLCP(onPerfEntry);
+      getTTFB(onPerfEntry);
+    });
   }
 };
 
